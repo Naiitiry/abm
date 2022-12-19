58c422114b661157f786f5c982ff9b239f5c42ed
Naiitiry <108165605+Naiitiry@users.noreply.github.com>
2022-12-19 10:11:11 -0300

    Update update.js

--- js/update.js
+++ js/update.js
@@ -32,7 +32,7 @@ function modificar() {
         .then(function () {
             console.log("modificado")
             alert("Registro modificado")
-            window.location.href = "./abm/index.html";  
+            window.location.href = "./index.html";  
         //NUEVO,  si les da error el fetch  comentar esta linea que puede dar error  
         })
         .catch(err => {
